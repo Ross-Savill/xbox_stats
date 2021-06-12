00133c70b9e25c4d3bb3c663ab168646243fe595
Ross Savill <rosssavill252@hotmail.com>
2021-06-12 23:50:47 +0100

    adding form functionality

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,4 +2,13 @@ class StatsController < ApplicationController
   def index
     @stats = Stat.all
   end
+
+  def search
+    stat = find_stat(params[:stat])
+    unless stat
+      flash[:alert] = 'Stats not found'
+      return render action: :index
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'stats#index'
   get '/stats', to: 'stats#index'
   get '/stats/:id', to: 'stats#show'
+  get '/search' => 'stats#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
