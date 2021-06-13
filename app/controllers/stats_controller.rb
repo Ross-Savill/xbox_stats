c0b3208d0266c2b23d753356a42d83feac574711
Ross Savill <rosssavill252@hotmail.com>
2021-06-13 23:46:06 +0100

    Bringing across json data from xbox api

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,11 +4,43 @@ class StatsController < ApplicationController
   end
 
   def search
-    stat = find_stat(params[:stat])
-    unless stat
-      flash[:alert] = 'Stats not found'
+    username = find_user(params[:username])
+
+    unless username
+      flash[:alert] = 'No Username Match'
       return render action: :index
     end
+
+    @user = username
+    find_stats(@user['profileUsers'])
   end
+
+  def find_stats(userData)
+    @userId = userData[0].first[1]
+    user_game_data = request_api("https://xbl.io/api/v2/achievements/player/#{URI.encode(@userId)}")
+    @all_games = user_game_data['titles']
+  end
+
+
+  private
+
+  def request_api(url)
+    response = Excon.get(
+      url,
+      headers: {
+        'X-Authorization' => ENV.fetch('XBOX_AUTH_KEY')
+      }
+    )
+    return nil if response.status != 200
+    JSON.parse(response.body)
+  end
+
+  def find_user(username)
+    request_api(
+      "https://xbl.io/api/v2/friends/search?gt=#{URI.encode(username)}"
+    )
+  end
+
+
 
 end
