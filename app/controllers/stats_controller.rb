ff1094d67a3bea1ff0dbfb8c030d248866450310
Ross Savill <rosssavill252@hotmail.com>
2021-06-14 22:28:13 +0100

    show page for each game added

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,26 +1,34 @@
 class StatsController < ApplicationController
+
   def index
     @stats = Stat.all
   end
 
   def search
-    username = find_user(params[:username])
+    find_user(params[:username])
 
-    unless username
+    unless @user
       flash[:alert] = 'No Username Match'
       return render action: :index
     end
-
-    @user = username
     find_stats(@user['profileUsers'])
+  end
+
+  def show
+    game_id = params[:gameId]
+    user_id = params[:userId]
+    find_user_game(user_id, game_id)
   end
 
   def find_stats(userData)
     @userId = userData[0].first[1]
-    user_game_data = request_api("https://xbl.io/api/v2/achievements/player/#{URI.encode(@userId)}")
+    user_id_in_state(@userId)
+    @xbox_gamertag = userData[0]['settings'][2]['value']
+    user_game_data = find_user_game_data(@userId)
     @all_games = user_game_data['titles']
+    @total_achievements = @all_games.reduce(0) { |sum, obj| sum + obj['achievement']['currentAchievements']}
+    @total_gamerscore = @all_games.reduce(0) { |sum, obj| sum + obj['achievement']['currentGamerscore']}
   end
-
 
   private
 
@@ -36,11 +44,20 @@ class StatsController < ApplicationController
   end
 
   def find_user(username)
-    request_api(
-      "https://xbl.io/api/v2/friends/search?gt=#{URI.encode(username)}"
-    )
+    @user = request_api("https://xbl.io/api/v2/friends/search?gt=#{URI.encode(username)}")
+    @userId = @user['profileUsers'][0].first[1]
   end
 
+  def find_user_game_data(userId)
+    request_api("https://xbl.io/api/v2/achievements/player/#{URI.encode(userId)}")
+  end
 
+  def user_id_in_state(id)
+    @stored_user_id = id
+  end
+
+  def find_user_game(user_id, game_id)
+    @selected_game = request_api("https://xbl.io/api/v2/achievements/player/#{URI.encode(user_id)}/title/#{URI.encode(game_id)}")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'stats#index'
   get '/stats', to: 'stats#index'
-  get '/stats/:id', to: 'stats#show'
-  get '/search' => 'stats#search'
+  get '/stats/:id', to: 'stats#show', as: "show_path"
+  get '/search', to: 'stats#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
